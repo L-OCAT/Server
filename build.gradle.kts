7b17a1a54412e1f9b52cd3548c31261b02866f3d
SeongHo5 <newfuring1123@gmail.com>
2024-07-07 22:23:07 +0900

    Init project

---
+++ build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+    id("com.diffplug.spotless") version "6.25.0"
+}
+
+group = "com.locat"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation(platform("software.amazon.awssdk:bom:2.21.0"))
+    implementation("software.amazon.awssdk:dynamodb-enhanced")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.projectlombok:lombok")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // Local Development
+    runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
+
+    // Testing
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.mockito:mockito-junit-jupiter")
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.testcontainers:mysql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+}
+
+spotless {
+    java {
+        importOrder(
+            "java|javax|jakarta",
+            "org.springframework",
+            "lombok",
+            "com.locat",
+            "",
+            "org.junit|org.mockito",
+            "\\#",
+            "\\#org.junit|org.assertj"
+        )
+        endWithNewline()
+        formatAnnotations()
+        removeUnusedImports()
+        trimTrailingWhitespace()
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    reports.html.required.set(false)
+    reports.junitXml.required.set(false)
+    maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).coerceAtLeast(1)
+}
+
---
+++ src/test/java/com/locat/server/ServerApplicationTest.java
@@ -0,0 +1,14 @@
+package com.locat.server;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class ServerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
