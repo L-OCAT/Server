6bcac999bd636d827a2db552b098a59ebb6b3887
SeongHo5 <newfuring1123@gmail.com>
2024-08-04 21:54:04 +0900

    Feat(LOCAT-BE-20): Update application setting
    - EnableConfigurationProperties 어노테이션 추가
    - Properties 에 OAuth2 관련 추가

--- src/main/java/com/locat/api/ServerApplication.java
+++ src/main/java/com/locat/api/ServerApplication.java
@@ -4,10 +4,12 @@ import com.locat.api.global.config.TimeConfig;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class ServerApplication {
 
   public static void main(String[] args) {
