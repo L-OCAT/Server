6eab028f4711911d610bd9ca01485473cab97db1
SeongHo5 <newfuring1123@gmail.com>
2024-08-12 21:14:18 +0900

    Feat(LOCAT-BE-20): Update redis config
    - 레디스 관련 도메인 RedisHash 사용하도록 일관화

--- src/main/java/com/locat/api/infrastructure/redis/config/RedisConfig.java
+++ src/main/java/com/locat/api/infrastructure/redis/config/RedisConfig.java
@@ -3,18 +3,29 @@ package com.locat.api.infrastructure.redis.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
+@EnableRedisRepositories(basePackages = "com.locat.api")
 @RequiredArgsConstructor
 public class RedisConfig {
 
   private final RedisProperties redisProperties;
 
+  @Bean
+  public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory);
+    return redisTemplate;
+  }
+
   /** Lecttuce 클라이언트 Connection Factory 설정 */
   @Bean
-  protected LettuceConnectionFactory redisConnectionFactory() {
+  protected RedisConnectionFactory redisConnectionFactory() {
     final String host = redisProperties.getHost();
     final Integer port = redisProperties.getPort();
     RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
