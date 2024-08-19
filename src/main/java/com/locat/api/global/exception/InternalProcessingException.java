7b9fc9b1ca7207fccb1042c201cbe828fdc79b21
SeongHo5 <newfuring1123@gmail.com>
2024-08-19 23:20:24 +0900

    Feat(LOCAT-BE-26): Refactor DynamoDbAuditorAdvice
    - Current Auditor 정보 가져오기 실패한 경우는 오류이므로 예외 던지도록 변경

--- src/main/java/com/locat/api/global/exception/InternalProcessingException.java
+++ src/main/java/com/locat/api/global/exception/InternalProcessingException.java
@@ -2,6 +2,10 @@ package com.locat.api.global.exception;
 
 import lombok.Getter;
 
+/**
+ * 내부 로직 처리 중 예외 발생 시 던지는 예외<br>
+ * 예외 메시지를 로깅하고, 클라이언트에게는 500 에러를 반환한다.
+ */
 @Getter
 public class InternalProcessingException extends LocatApiException {
 
