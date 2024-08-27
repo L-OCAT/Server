6f051f0ef044f7a2e88ecb2275ff68f8af9e4b92
yxnxx <jh4rgh0@gmail.com>
2024-08-27 19:48:39 +0900

    Feat(LOCAT-BE-37): add userendpoint entity

---
+++ src/main/java/com/locat/api/domain/user/entity/Platform.java
@@ -0,0 +1,5 @@
+package com.locat.api.domain.user.entity;
+
+public enum Platform {
+    ios, android
+}
---
+++ src/main/java/com/locat/api/domain/user/entity/UserEndpoint.java
@@ -0,0 +1,34 @@
+package com.locat.api.domain.user.entity;
+
+import com.locat.api.domain.core.SecuredBaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "user_endpoint")
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEndpoint extends SecuredBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "int UNSIGNED not null")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "device_token", nullable = false)
+    private String deviceToken;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "platform", nullable = false)
+    private Platform platform;
+
+    @Column(name = "endpoint_arn", nullable = false)
+    private String endpointArn;
+
+}
