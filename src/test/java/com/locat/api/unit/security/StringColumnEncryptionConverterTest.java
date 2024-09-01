e020786035dededc5d9a9914e4cd70893b266414
조성호 <newfuring1123@gmail.com>
2024-09-01 21:58:58 +0900

    Feat(LOCAT-BE-43): 개인 정보 암호화 처리 (#14)
    * Feat(LOCAT-BE-43): Add & Apply Encryptor
    
    - 암/복호화 수행하는 Converter 구현
    - User 정보 중 개인 정보 컬럼에 대해 암호화 처리
    
    * Feat(LOCAT-BE-43): Refactor test to use reflection utils
    
    - Value 필드 주입을 직접 리플렉션 -> ReflectionTestUtils 사용으로 변경
    
    * Fix(LOCAT-BE-43): Fix typos
    
    - 오타 수정

---
+++ src/test/java/com/locat/api/unit/security/StringColumnEncryptionConverterTest.java
@@ -0,0 +1,68 @@
+package com.locat.api.unit.security;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.locat.api.global.security.StringColumnEncryptionConverter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@ExtendWith(MockitoExtension.class)
+class StringColumnEncryptionConverterTest {
+
+  @InjectMocks private StringColumnEncryptionConverter converter;
+
+  @BeforeEach
+  void setUp() {
+    ReflectionTestUtils.setField(this.converter, "encryptionKey", "testEncryptionKey123", String.class);
+    MockitoAnnotations.openMocks(this);
+    this.converter.init();
+  }
+
+  @Test
+  @DisplayName("평문 암호화에 성공해야 한다.")
+  void encryptStringtoDatabaseColumn() {
+    // Given
+    String plainText = "Hello, World!";
+
+    // When
+    String encrypted = converter.convertToDatabaseColumn(plainText);
+
+    // Then
+    assertThat(encrypted).isNotNull();
+    assertThat(encrypted).isNotEqualTo(plainText);
+  }
+
+  @Test
+  @DisplayName("암호문 복호화에 성공해야 한다.")
+  void decryptDatabaseColumnToPlain() {
+    // Given
+    String expected = "Hello, World!";
+    String encryptedExpect = converter.convertToDatabaseColumn(expected);
+
+    // When
+    String actual = converter.convertToEntityAttribute(encryptedExpect);
+
+    // Then
+    assertThat(actual).isNotNull();
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName("null이 주어지는 경우 null을 반환해야 한다.")
+  void returnNullWhenNullIsGiven() {
+    // Given & When
+    String encrypted = converter.convertToDatabaseColumn(null);
+    String decrypted = converter.convertToEntityAttribute(null);
+
+    // Then
+    assertThat(encrypted).isNull();
+    assertThat(decrypted).isNull();
+  }
+
+}
