c0886b249c5496c601288c4da37c5db8f6b59ce2
SeongHo5 <newfuring1123@gmail.com>
2024-09-08 21:43:42 +0900

    Docs(LOCAT-BE-46): Update comments
    - 주석 보강

--- src/main/java/com/locat/api/domain/faq/entity/FaqType.java
+++ src/main/java/com/locat/api/domain/faq/entity/FaqType.java
@@ -3,12 +3,15 @@ package com.locat.api.domain.faq.entity;
 import java.util.Arrays;
 
 public enum FaqType {
-  /** 일반 */
+  /** 공통 */
   GENERAL,
-
+  /** 회원 관련 */
   USER,
+  /** 분실물 & 습득물 관련 */
   LOST_FOUND,
+  /** 서비스 이용 관련 */
   SERVICE,
+  /** 그 외 기타 */
   MISCELLANEOUS,
   ;
 
