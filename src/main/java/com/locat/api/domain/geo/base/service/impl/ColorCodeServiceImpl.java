e43daad69d7dd3226a9682e70f57b1d2177872d1
SeongHo5 <newfuring1123@gmail.com>
2024-09-20 22:26:22 +0900

    Feat(LOCAT-BE-49): Add ColorCode API

--- src/main/java/com/locat/api/domain/geo/base/service/impl/ColorCodeServiceImpl.java
+++ src/main/java/com/locat/api/domain/geo/base/service/impl/ColorCodeServiceImpl.java
@@ -3,6 +3,7 @@ package com.locat.api.domain.geo.base.service.impl;
 import com.locat.api.domain.geo.base.entity.ColorCode;
 import com.locat.api.domain.geo.base.service.ColorCodeService;
 import com.locat.api.infrastructure.repository.geo.base.ColorCodeRepository;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,10 @@ public class ColorCodeServiceImpl implements ColorCodeService {
   @Transactional(readOnly = true)
   public Optional<ColorCode> findById(Long id) {
     return this.colorCodeRepository.findById(id);
+  }
+
+  @Override
+  public List<ColorCode> findAll() {
+    return this.colorCodeRepository.findAll();
   }
 }
