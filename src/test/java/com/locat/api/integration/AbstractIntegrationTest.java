a4db66222f2f0d90c15c25475aa7b7146ea1c1b5
SeongHo5 <newfuring1123@gmail.com>
2024-10-09 19:58:10 +0900

    Feat(LOCAT-BE-49): Add test properties

--- src/test/java/com/locat/api/integration/AbstractIntegrationTest.java
+++ src/test/java/com/locat/api/integration/AbstractIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.locat.api.integration;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AbstractIntegrationTest {}
