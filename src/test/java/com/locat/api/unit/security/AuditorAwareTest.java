cd694768bc25c0e2a80fb486b167ca4e5165c0f1
SeongHo5 <newfuring1123@gmail.com>
2024-11-08 16:58:50 +0900

    Feat(LOCAT-BE-50): Add test codes
    - security filter 생성 Factory 추가
    - 관리자 API 요청 로깅 기능 추가

--- src/test/java/com/locat/api/unit/security/AuditorAwareTest.java
+++ src/test/java/com/locat/api/unit/security/AuditorAwareTest.java
@@ -1,7 +1,6 @@
 package com.locat.api.unit.security;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import com.locat.api.global.auth.LocatUserDetails;
@@ -10,47 +9,46 @@ import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-@ExtendWith(MockitoExtension.class)
 class AuditorAwareTest {
 
-  @InjectMocks private LocatAuditorAware locatAuditorAware;
+  private static final String TEST_USER_NAME = "test-user";
+
+  @InjectMocks private LocatAuditorAware auditorAware;
   @Mock private SecurityContext securityContext;
   @Mock private Authentication authentication;
   @Mock private LocatUserDetails locatUserDetails;
 
   @BeforeEach
   void setUp() {
+    MockitoAnnotations.openMocks(this);
     SecurityContextHolder.setContext(this.securityContext);
   }
 
   @Test
-  @DisplayName("인증된 사용자라면, 현재 사용자의 ID를 반환해야 한다.")
+  @DisplayName("인증된 사용자라면, 현재 사용자의 ID / 이름을 반환해야 한다.")
   void whenAuthenticatedShouldReturnUserId() {
     // Given
     Long userId = 123L;
     when(this.securityContext.getAuthentication()).thenReturn(this.authentication);
     when(this.authentication.isAuthenticated()).thenReturn(true);
     when(this.authentication.getPrincipal()).thenReturn(this.locatUserDetails);
+    when(this.authentication.getName()).thenReturn(TEST_USER_NAME);
     when(this.locatUserDetails.getId()).thenReturn(userId);
 
     // When
-    Optional<Long> currentAuditor = this.locatAuditorAware.getCurrentAuditor();
+    Optional<Long> currentAuditor = this.auditorAware.getCurrentAuditor();
+    Optional<String> currentAuditorName = this.auditorAware.getCurrentAuditorName();
 
     // Then
     assertThat(currentAuditor).isPresent().hasValue(userId);
-    assertAll(
-        () -> verify(this.securityContext, times(1)).getAuthentication(),
-        () -> verify(this.authentication, times(1)).isAuthenticated(),
-        () -> verify(this.authentication, times(1)).getPrincipal(),
-        () -> verify(this.locatUserDetails, times(1)).getId());
+    assertThat(currentAuditorName).isPresent().hasValue(TEST_USER_NAME);
   }
 
   @Test
@@ -61,13 +59,12 @@ class AuditorAwareTest {
     when(this.authentication.isAuthenticated()).thenReturn(false);
 
     // When
-    Optional<Long> currentAuditor = this.locatAuditorAware.getCurrentAuditor();
+    Optional<Long> currentAuditor = this.auditorAware.getCurrentAuditor();
+    Optional<String> currentAuditorName = this.auditorAware.getCurrentAuditorName();
 
     // Then
     assertThat(currentAuditor).isEmpty();
-    assertAll(
-        () -> verify(this.securityContext, times(1)).getAuthentication(),
-        () -> verify(this.authentication, times(1)).isAuthenticated());
+    assertThat(currentAuditorName).isEmpty();
   }
 
   @Test
@@ -77,10 +74,11 @@ class AuditorAwareTest {
     when(this.securityContext.getAuthentication()).thenReturn(null);
 
     // When
-    Optional<Long> currentAuditor = this.locatAuditorAware.getCurrentAuditor();
+    Optional<Long> currentAuditor = this.auditorAware.getCurrentAuditor();
+    Optional<String> currentAuditorName = this.auditorAware.getCurrentAuditorName();
 
     // Then
     assertThat(currentAuditor).isEmpty();
-    assertAll(() -> verify(this.securityContext, times(1)).getAuthentication());
+    assertThat(currentAuditorName).isEmpty();
   }
 }
